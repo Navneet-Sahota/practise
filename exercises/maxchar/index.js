da9c20368d896583ba04451d513bbe1f203d592b
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-21 04:12:08 +0530

    solve maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+	const myMap = new Map();
+	let max = 0;
+	let maxChar = "";
+	for (let char of str) {
+		if (!myMap.has(char)) {
+			myMap.set(char, 1);
+		} else {
+			myMap.set(char, myMap.get(char) + 1);
+		}
+	}
+	for (let [char, freq] of myMap) {
+		if (max < freq) {
+			max = freq;
+			maxChar = char;
+		}
+	}
+	return maxChar;
+}
 module.exports = maxChar;
