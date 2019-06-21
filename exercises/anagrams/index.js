376adb81b1e04d30623d3ee39af46b2130c8643f
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-21 21:06:07 +0530

    solve anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,59 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	stringA = stringA
+		.replace(/\W/g, "")
+		.toLowerCase()
+		.split("")
+		.sort()
+		.join("");
+	stringB = stringB
+		.replace(/\W/g, "")
+		.toLowerCase()
+		.split("")
+		.sort()
+		.join("");
+
+	if (stringA === stringB) {
+		return true;
+	}
+	return false;
+
+	// Alternate Method
+
+	// stringA = stringA.replace(/\W/g, "").toLowerCase();
+	// stringB = stringB.replace(/\W/g, "").toLowerCase();
+	// if (stringA.length !== stringB.length) {
+	// 	return false;
+	// }
+	// const stringAMap = new Map();
+	// const stringBMap = new Map();
+
+	// populateMap(stringAMap, stringA);
+	// populateMap(stringBMap, stringB);
+
+	// if (stringAMap.size !== stringBMap.size) {
+	// 	return false;
+	// }
+
+	// for (let [key] of stringAMap) {
+	// 	if (!stringAMap.get(key) === stringBMap.get(key)) {
+	// 		return false;
+	// 	}
+	// }
+
+	// return true;
+}
+
+// function populateMap(map, string) {
+// 	for (let char of string) {
+// 		if (!map.has(char)) {
+// 			map.set(char, 1);
+// 		} else {
+// 			map.set(char, map.get(char) + 1);
+// 		}
+// 	}
+// }
 
 module.exports = anagrams;
