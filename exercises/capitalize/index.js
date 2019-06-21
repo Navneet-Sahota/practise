89bd369e6bb7ae364621077211f6eded718d49a7
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-21 21:29:48 +0530

    solve capitalize problem

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+	return str
+		.split(" ")
+		.map(word => {
+			let capitalizedWord = word.slice(0, 1);
+			capitalizedWord = capitalizedWord.toUpperCase() + word.slice(1);
+			return capitalizedWord;
+		})
+		.join(" ");
+}
 
 module.exports = capitalize;
