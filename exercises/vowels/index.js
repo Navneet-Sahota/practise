cf8b9fb931e63635a3d47b91072387d066108db8
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-24 03:13:08 +0530

    solve vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,13 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
+
+	// const vowelsString = "aeiou";
+	// return str.split("").filter(char => vowelsString.includes(char.toLowerCase()))
+	// 	.length;
+}
 
 module.exports = vowels;
