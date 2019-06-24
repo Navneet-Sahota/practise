6c5fea9e9a57d576b5d86bfe5e8f61ab46065934
Navneet Sahota <navneet.sahota@outlook.com>
2019-06-24 05:51:51 +0530

    solve fib problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+	const cache = {};
+	return function(...args) {
+		if (cache[args]) {
+			return cache[args];
+		}
+		const result = fn(...args);
+		cache[args] = result;
+
+		return result;
+	};
+}
+
+const fib = memoize(fibSlow);
+
+function fibSlow(n) {
+	// RECURSIVE SOLUTION
+
+	if (n < 2) {
+		return n;
+	}
+
+	return fib(n - 1) + fib(n - 2);
+
+	// ITERATIVE SOLUTION
+
+	// if (n === 0) {
+	// 	return 0;
+	// }
+	// if (n <= 2) {
+	// 	return 1;
+	// }
+	// let a = 0;
+	// let b = 1;
+	// let c = a + b;
+	// for (let i = 2; i < n; i += 1) {
+	// 	a = b;
+	// 	b = c;
+	// 	c = a + b;
+	// }
+	// return c;
+}
 
 module.exports = fib;
